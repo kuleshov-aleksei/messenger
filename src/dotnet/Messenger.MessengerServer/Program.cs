696a4acfdac48c27551cb86b3e49534bf6a5de4c
Kuleshov A.V <avkuleshov@activebc.ru>
2020-10-06 20:48:03 +0300

    Create dotnet project structure

---
+++ src/dotnet/Messenger.MessengerServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Messenger.MessengerServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
