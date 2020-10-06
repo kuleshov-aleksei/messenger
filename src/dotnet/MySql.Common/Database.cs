cea2e40939a8bd938363cba3dff0245624d89487
Kuleshov A.V <avkuleshov@activebc.ru>
2020-10-06 22:25:06 +0300

    Create mysql connection

---
+++ src/dotnet/MySql.Common/Database.cs
@@ -0,0 +1,53 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace MySql.Common
+{
+    public class Database
+    {
+        private MySqlConnection m_mySqlConnection;
+
+        public Database(DatabaseConnectionSettings databaseConnectionSettings)
+        {
+            string connectionString = 
+                $"server={databaseConnectionSettings.Host};" +
+                $"port={databaseConnectionSettings.Port};" +
+                $"database={databaseConnectionSettings.DatabaseName};" +
+                $"user={databaseConnectionSettings.User};" +
+                $"password={databaseConnectionSettings.Password}";
+
+            m_mySqlConnection = new MySqlConnection(connectionString);
+
+            try
+            {
+                m_mySqlConnection.Open();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public void ExecuteSql(string sql, Action<IDataReader> onRow = null)
+        {
+            MySqlCommand command = m_mySqlConnection.CreateCommand();
+            command.CommandText = sql;
+
+            if (onRow == null)
+            {
+                command.ExecuteNonQuery();
+            }
+            else
+            {
+                using (MySqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        onRow(reader);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ src/dotnet/MySql.Common/DatabaseConnection.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace MySql.Common
+{
+    public class DatabaseConnectionSettings
+    {
+        [JsonProperty("host")]
+        public string Host { get; set; }
+
+        [JsonProperty("port")]
+        public int Port { get; set; }
+
+        [JsonProperty("database_name")]
+        public string DatabaseName { get; set; }
+
+        [JsonProperty("user")]
+        public string User { get; set; }
+
+        [JsonProperty("password")]
+        public string Password { get; set; }
+
+        public static DatabaseConnectionSettings ReadFromFile(string jsonFilename)
+        {
+            if (!File.Exists(jsonFilename))
+            {
+                throw new ApplicationException("File does not exists");
+            }
+
+            return JsonConvert.DeserializeObject<DatabaseConnectionSettings>(File.ReadAllText(jsonFilename));
+        }
+    }
+}
