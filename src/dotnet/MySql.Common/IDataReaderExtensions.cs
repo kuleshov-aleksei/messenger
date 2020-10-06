242992f9967ef164e741dbfe0c2bb394e2632b2e
Aleksei <alexxey67@gmail.com>
2020-10-06 22:25:06 +0300

    Create mysql connection

---
+++ src/dotnet/MySql.Common/IDataReaderExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+
+namespace MySql.Common
+{
+    public static class IDataReaderExtensions
+    {
+        public static string GetString(this IDataRecord record, string fieldName)
+        {
+            int fieldId = record.GetOrdinal(fieldName);
+
+            if (record.IsDBNull(fieldId))
+            {
+                return null;
+            }
+
+            return record.GetString(fieldId);
+        }
+
+        public static DateTime? GetDateTime(this IDataRecord record, string fieldName)
+        {
+            int fieldId = record.GetOrdinal(fieldName);
+
+            if (record.IsDBNull(fieldId))
+            {
+                return null;
+            }
+
+            return record.GetDateTime(fieldId);
+        }
+
+    }
+}
