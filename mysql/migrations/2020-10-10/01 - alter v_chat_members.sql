82caa02e508978d92cd8b0048c9b724422b985a2
Aleksei <alexxey67@gmail.com>
2020-10-10 13:39:22 +0300

    Alter view

---
+++ mysql/migrations/2020-10-10/01 - alter v_chat_members.sql
@@ -0,0 +1,21 @@
+USE `dcsm`;
+CREATE 
+     OR REPLACE ALGORITHM = UNDEFINED 
+    DEFINER = `dcsm`@`%` 
+    SQL SECURITY DEFINER
+VIEW `dcsm`.`v_chat_members` AS
+    SELECT 
+        `dcsm`.`chat_members`.`chat_id` AS `id`,
+        `dcsm`.`chat_members`.`joined_at` AS `joined_at`,
+        `dcsm`.`chat_members`.`chat_id` AS `chat_id`,
+        `member`.`id` AS `user_id`,
+        `member`.`username` AS `username`,
+        `member`.`name` AS `name`,
+        `member`.`surname` AS `surname`,
+        `inviter`.`username` AS `invited_by_username`,
+        `inviter`.`name` AS `invited_by_name`,
+        `inviter`.`surname` AS `invited_by_surname`
+    FROM
+        ((`dcsm`.`chat_members`
+        JOIN `dcsm`.`user` `member` ON ((`dcsm`.`chat_members`.`user_id` = `member`.`id`)))
+        JOIN `dcsm`.`user` `inviter` ON ((`dcsm`.`chat_members`.`added_by` = `inviter`.`id`)));
