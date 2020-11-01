5fbf2b2e1a239c1c55ed1a9c2ddc29d62216cbbd
Aleksei <alexxey67@gmail.com>
2020-11-01 22:41:01 +0300

    Implement service status component

--- src/web/src/views/Admin.vue
+++ src/web/src/views/Admin.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="admin">
-    <h1>Администрирование</h1>
+    <el-tabs v-model="activeTabName">
+      <el-tab-pane label="Статус сервисов" name="status">
+        <ServiceStatus />
+      </el-tab-pane>
+      <el-tab-pane label="Пользователи" name="users">Пользователи</el-tab-pane>
+      <el-tab-pane label="Чаты" name="chats">Чаты</el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
@@ -13,10 +19,14 @@ import sessionStore from "../sessionStore";
 export default {
     data() {
       return {
+        activeTabName: 'status'
       };
     },
+    components: {
+      ServiceStatus: () => import("../components/ServiceStatus.vue"),
+    },
     methods: {
-        
+
     },
     mounted() {
       store.commit('save_current_route', '/admin');
@@ -33,6 +43,22 @@ export default {
   };
 </script>
 
-<style>
+<style lang="scss">
+@import "../styles/variables.scss";
+
+.admin {
+  margin-top: 10px;
+  min-width: 50%;
+  margin: 10px auto 0 auto;
+  padding: 10px;
+  background: white;
+  border-radius: 4px;
+  box-shadow: 0 1px 0 0 var(--steel_gray_120),0 0 0 1px var(--steel_gray_80);
+
+  @include respond-to(mobile) { max-width: 100%; }
+  @include respond-to(medium-screens) { max-width: 100%; }
+  @include respond-to(regular) { max-width: 60% }
+  @include respond-to(wide-screens) { max-width: 50% }
+}
 
 </style>
