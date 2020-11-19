a0a230ff94d9d9cdffe983ee13b4ce84ace95dc8
Aleksei <alexxey67@gmail.com>
2020-11-20 00:09:12 +0300

    Add settings for rabbit mq

---
+++ mysql/migrations/2020-11-19/01 - add settings for rabbit mq.sql
@@ -0,0 +1,3 @@
+INSERT INTO `dcsm`.`settings` (`id`, `name`, `value`, `description`) VALUES ('8', 'rabbit_mq_address', '192.168.40.76', 'Rabbit MQ address');
+INSERT INTO `dcsm`.`settings` (`name`, `value`, `description`) VALUES ('rabbit_mq_user', 'services', 'Rabbit MQ username');
+INSERT INTO `dcsm`.`settings` (`name`, `value`, `description`) VALUES ('rabbit_mq_password', 'l9MqLHC6ca', 'Rabbit MQ password');
