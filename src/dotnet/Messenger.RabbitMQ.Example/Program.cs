5ff7a6751658ac1bca2ab39b77f12947583e2c92
Aleksei <alexxey67@gmail.com>
2020-11-20 00:22:13 +0300

    Create sample application for mq

---
+++ src/dotnet/Messenger.RabbitMQ.Example/Program.cs
@@ -0,0 +1,32 @@
+﻿using Messenger.RabbitMQ.Common;
+using NLog;
+using System.Threading;
+
+namespace Messenger.RabbitMQ.Example
+{
+    class Program
+    {
+        private static Logger m_logger = LogManager.GetCurrentClassLogger();
+
+        static void Main(string[] args)
+        {
+            m_logger.Info("Starting");
+
+            using ProducerWrapper producerWrapper = new ProducerWrapper(QueueName.NewUser);
+            using ConsumerWrapper consumerWrapper = new ConsumerWrapper(QueueName.NewUser);
+
+            consumerWrapper.OnString += (message) =>
+            {
+                m_logger.Info($"Got message: \"{message}\"");
+            };
+
+            producerWrapper.Produce("Hello world!");
+            producerWrapper.Produce("Test message");
+
+            Thread.Sleep(500);
+            producerWrapper.Produce("Message after sleep");
+
+            Thread.Sleep(500);
+        }
+    }
+}
