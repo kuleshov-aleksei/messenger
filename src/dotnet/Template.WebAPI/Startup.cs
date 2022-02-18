7976f9efa701c81039891f7cb91da242cb57546a
kuleshov-aleksei <alexxey67@gmail.com>
2022-02-18 20:54:30 +0300

    Update template

--- src/dotnet/Template.WebAPI/Startup.cs
+++ src/dotnet/Template.WebAPI/Startup.cs
@@ -1,3 +1,5 @@
+using Messenger.Common.JWT;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -5,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
