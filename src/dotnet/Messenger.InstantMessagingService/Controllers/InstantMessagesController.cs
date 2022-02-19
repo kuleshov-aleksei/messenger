d7be03bd7d8481947f75b57f6d6955b01f554dba
kuleshov-aleksei <alexxey67@gmail.com>
2022-02-19 16:07:33 +0300

    Add endpoint for receiving messages

---
+++ src/dotnet/Messenger.InstantMessagingService/Controllers/InstantMessagesController.cs
@@ -0,0 +1,53 @@
+﻿using Messenger.Common.JWT;
+using Messenger.Common.Settings;
+using Messenger.InstantMessagingService.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MySql.Common;
+using NLog;
+using StackExchange.Redis.Extensions.Core.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Messenger.InstantMessagingService.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("/messenger/instant")]
+    public class InstantMessagesController : ControllerBase
+    {
+        private readonly Logger m_logger = LogManager.GetCurrentClassLogger();
+        private readonly IRedisDatabase m_redis;
+
+        public InstantMessagesController(IRedisDatabase redisDatabase)
+        {
+            m_redis = redisDatabase;
+        }
+
+        [HttpPost("send")]
+        public async Task SendMessage(SendMessageRequest request)
+        {
+            int userId = JwtHelper.GetUserId(User.Claims);
+            m_logger.Trace($"User {userId} is sending message to chat {request.ChatId}");
+
+            List<int> userChats = await GlobalSettings.Instance.Database.GetUserChatListAsync(m_redis, userId);
+            if (!userChats.Contains(request.ChatId))
+            {
+                HttpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+                return;
+            }
+
+            if (string.IsNullOrEmpty(request.Message))
+            {
+                m_logger.Debug("Message is empty. Rejecting");
+                HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+                return;
+            }
+
+            m_logger.Trace($"TEMP: {request.Message}");
+        }
+    }
+}
---
+++ src/dotnet/Messenger.InstantMessagingService/Models/SendMessageRequest.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace Messenger.InstantMessagingService.Models
+{
+    public class SendMessageRequest
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+
+        [JsonProperty("chat_id")]
+        public int ChatId { get; set; }
+    }
+}
