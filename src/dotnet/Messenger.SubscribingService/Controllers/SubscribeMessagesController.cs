33d1f025bcf83015a87a469cf192ae1fc0a77ca3
kuleshov-aleksei <alexxey67@gmail.com>
2022-02-20 00:25:32 +0300

    Create subscription service

---
+++ src/dotnet/Messenger.SubscribingService/Controllers/SubscribeMessagesController.cs
@@ -0,0 +1,65 @@
+﻿using Messenger.SubscribingService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using System;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Messenger.SubscribingService.Controllers
+{
+    //[Authorize]
+    [ApiController]
+    [Route("/messenger/subscribe")]
+    public class SubscribeMessagesController : ControllerBase
+    {
+        private readonly Logger m_logger = LogManager.GetCurrentClassLogger();
+
+        public SubscribeMessagesController()
+        {
+        }
+
+        [HttpGet("ws")]
+        public async Task SubscribeWs()
+        {
+            if (HttpContext.WebSockets.IsWebSocketRequest)
+            {
+                m_logger.Info($"Client connected over websockets");
+                using var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+                await Echo(webSocket);
+            }
+            else
+            {
+                m_logger.Info($"Client tried to connect to ws endpoint, but connection isn't websocket");
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+
+        private static async Task Echo(WebSocket webSocket)
+        {
+            var buffer = new byte[1024 * 4];
+            var receiveResult = await webSocket.ReceiveAsync(
+                new ArraySegment<byte>(buffer), CancellationToken.None);
+
+            while (!receiveResult.CloseStatus.HasValue)
+            {
+                await webSocket.SendAsync(
+                    new ArraySegment<byte>(buffer, 0, receiveResult.Count),
+                    receiveResult.MessageType,
+                    receiveResult.EndOfMessage,
+                    CancellationToken.None);
+
+                receiveResult = await webSocket.ReceiveAsync(
+                    new ArraySegment<byte>(buffer), CancellationToken.None);
+            }
+
+            await webSocket.CloseAsync(
+                receiveResult.CloseStatus.Value,
+                receiveResult.CloseStatusDescription,
+                CancellationToken.None);
+        }
+    }
+}
---
+++ src/dotnet/Messenger.SubscribingService/Program.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Messenger.SubscribingService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+#if DEBUG
+            if (Directory.Exists("bin/Debug/net6.0"))
+            {
+                Directory.SetCurrentDirectory("bin/Debug/net6.0");
+            }
+#endif
+
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();
+    }
+}
